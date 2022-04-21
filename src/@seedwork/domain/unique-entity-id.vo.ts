ba906de713c270517ed876a643133467fb121c0d
Alpiona <alpiona@hotmail.com>
2022-04-21 17:59:53 -0300

    UUID object value created

---
+++ src/@seedwork/domain/unique-entity-id.vo.ts
@@ -0,0 +1,15 @@
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
+import InvalidUuidError from "../errors/invalid-uuid.error";
+
+export default class UniqueEntityId {
+  constructor(public readonly id?: string) {
+    this.id = this.id || uuidv4();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.id);
+    if (!isValid) {
+      throw new InvalidUuidError();
+    }
+  }
+}
--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,8 +1,68 @@
 import Category from "./category";
+import { omit } from "lodash";
+import UniqueEntityId from "../../../@seedwork/domain/unique-entity-id.vo";
 
 describe("Category Tests", () => {
   test("Constructor of category", () => {
-    const category = new Category("Movie");
-    expect(category.name).toBe("Movie");
+    let category = new Category({ name: "Movie" });
+
+    const props = omit(category.props, "created_at");
+
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+    });
+
+    const created_at = new Date();
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+  });
+
+  test("id field", () => {
+    const data = [
+      { props: { name: "Movie" } },
+      { props: { name: "Movie" }, id: null },
+      { props: { name: "Movie" }, id: undefined },
+      { props: { name: "Movie" }, id: new UniqueEntityId() },
+    ];
+
+    data.forEach((i) => {
+      const category = new Category(i.props, i.id);
+      expect(category.id).not.toBeNull();
+      expect(category.id).toBeInstanceOf(UniqueEntityId);
+    });
   });
 });
