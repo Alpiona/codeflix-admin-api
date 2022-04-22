f2897d5f8e3747367c77c050c35639094e9f5d88
Alpiona <alpiona@hotmail.com>
2022-04-22 17:15:36 +0000

    Added imutability to valueObject

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -10,4 +10,28 @@ describe("ValueObject Unit Tests", () => {
     vo = new StubValueObject({ prob1: "value1" });
     expect(vo.value).toStrictEqual({ prob1: "value1" });
   });
+
+  it("should convert to string", () => {
+    const date = new Date();
+    const arrange = [
+      { received: null, expected: "null" },
+      { received: undefined, expected: "undefined" },
+      { received: "", expected: "" },
+      { received: "fake test", expected: "fake test" },
+      { received: 0, expected: "0" },
+      { received: 1, expected: "1" },
+      { received: true, expected: "true" },
+      { received: false, expected: "false" },
+      { received: date, expected: date.toString() },
+      {
+        received: { prop1: "teste" },
+        expected: JSON.stringify({ prop1: "teste" }),
+      },
+    ];
+
+    arrange.forEach((value) => {
+      let vo = new StubValueObject(value.received);
+      expect(vo + "").toBe(value.expected);
+    });
+  });
 });
