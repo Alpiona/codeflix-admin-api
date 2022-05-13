74356eaa9d5a6cc04fe9802442fc51e1d729ff4b
Andre Piona <alpiona@hotmail.com>
2022-05-13 19:42:37 +0000

    Add update with service and tests

---
+++ src/category/application/use-cases/update-category.use-case.ts
@@ -0,0 +1,32 @@
+import UseCase from "../../../@seedwork/application/use-case";
+import Category from "../../domain/entities/category";
+import {CategoryRepository} from "../../domain/repository/category.repository";
+import { CategoryOutput, CategoryOutputMapper } from "../dto/category-output";
+
+export default class UpdateCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepo: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = await this.categoryRepo.findById(input.id)
+    entity.update(input.name, input.description)
+
+    if (input.is_active === true){
+      entity.activate()
+    } else if (input.is_active === false) {
+      entity.deactivate()
+    }
+
+    await this.categoryRepo.update(entity);
+    
+    return CategoryOutputMapper.toOutput(entity);
+  }
+}
+
+export type Input = {
+  id: string;
+  name: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type Output = CategoryOutput;
