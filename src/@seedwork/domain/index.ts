353162604d2d7652c466a5ce321df0eb181708f9
Alpiona <alpiona@hotmail.com>
2022-05-15 20:50:01 +0000

    Add command and script to create indexes

---
+++ src/@seedwork/domain/index.ts
@@ -0,0 +1,6 @@
+// created from 'create-ts-index'
+
+export * from './entity';
+export * from './repository';
+export * from './utils';
+export * from './value-objects';
---
+++ src/category/application/use-cases/index.ts
@@ -0,0 +1,6 @@
+// created from 'create-ts-index'
+
+export * from './create-category.use-case';
+export * from './get-category.use-case';
+export * from './list-categories.use-case';
+export * from './update-category.use-case';
