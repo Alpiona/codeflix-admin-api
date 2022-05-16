405d2faea7a1ed831ca80705d0ec2b81702386b3
Andre Piona <alpiona@hotmail.com>
2022-05-16 20:09:00 +0000

    Importing useCases first steps

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -11,7 +11,7 @@ export default {
   "rootDir": "src",
   "testRegex": ".*\\.spec\\.ts$",
   "transform": {
-    "^.+\\.(t|j)s$": "ts-jest"
+    "^.+\\.(t|j)s$": "@swc/jest"
   },
   "collectCoverageFrom": [
     "**/*.(t|j)s"
---
+++ src/nestjs/src/categories/categories.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import {CategoryInMemoryRepository} from 'core/category/infra'
+import CategoryRepository from 'core/dist/category/domain/repository/category.repository';
+import {CreateCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase} from 'core/category/application'
+
+@Module({
+  controllers: [CategoriesController],
+  providers: [CategoriesService, 
+    {
+    provide: 'CategoryRepository', useClass: CategoryInMemoryRepository,
+    },
+    {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository)=> {
+        return new CreateCategoryUseCase.UseCase(categoryRepo)
+      },
+      inject: ['CategoryRepository']
+    },
+    {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository)=> {
+        return new ListCategoriesUseCase.UseCase(categoryRepo)
+      },
+      inject: ['CategoryRepository']
+    },
+    {
+      provide: UpdateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository)=> {
+        return new UpdateCategoryUseCase.UseCase(categoryRepo)
+      },
+      inject: ['CategoryRepository']
+    },
+    {
+      provide: GetCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository)=> {
+        return new GetCategoryUseCase.UseCase(categoryRepo)
+      },
+      inject: ['CategoryRepository']
+    },
+]
+})
+export class CategoriesModule {}
