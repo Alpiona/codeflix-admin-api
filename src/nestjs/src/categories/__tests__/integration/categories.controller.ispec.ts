a131713010aaa875ae8e2e934926677697084f95
Andre Piona <alpiona@hotmail.com>
2022-10-03 22:31:17 +0000

    Add test for controller defined

--- src/nestjs/src/categories/__tests__/integration/categories.controller.ispec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller.ispec.ts
@@ -1,4 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from 'core/category/application';
+import { CategoryRepository } from 'core/category/domain';
+import { CATEGORY_PROVIDERS } from '../../../categories/category.providers';
 import { CategoriesController } from '../../../categories/categories.controller';
 import { CategoriesModule } from '../../../categories/categories.module';
 import { ConfigModule } from '../../../config/config.module';
@@ -6,6 +15,7 @@ import { DatabaseModule } from '../../../database/database.module';
 
 describe('CategoriesController Integration Tests', () => {
   let controller: CategoriesController;
+  let repository: CategoryRepository.Repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +23,46 @@ describe('CategoriesController Integration Tests', () => {
     }).compile();
 
     controller = module.get(CategoriesController);
+    repository = module.get(
+      CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+    );
   });
 
-  it('xpto', () => {
-    console.log(controller);
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(controller['createUseCase']).toBeInstanceOf(
+      CreateCategoryUseCase.UseCase,
+    );
+    expect(controller['deleteUseCase']).toBeInstanceOf(
+      DeleteCategoryUseCase.UseCase,
+    );
+    expect(controller['listUseCase']).toBeInstanceOf(
+      ListCategoriesUseCase.UseCase,
+    );
+    expect(controller['getUseCase']).toBeInstanceOf(GetCategoryUseCase.UseCase);
+    expect(controller['updateUseCase']).toBeInstanceOf(
+      UpdateCategoryUseCase.UseCase,
+    );
+  });
+
+  it('should create a category', async () => {
+    const output = await controller.create({
+      name: 'Movie',
+    });
+    const entity = await repository.findById(output.id);
+
+    expect(entity).toMatchObject({
+      id: output.id,
+      name: 'Movie',
+      description: null,
+      is_active: true,
+      created_at: output.created_at,
+    });
+
+    expect(output.id).toBe(entity.id);
+    expect(output.name).toBe('Movie');
+    expect(output.description).toBeNull();
+    expect(output.is_active).toBeTruthy();
+    expect(output.created_at).toStrictEqual(entity.created_at);
   });
 });
